fc9ffc9132f5e308a685c6f0f5f0a0bb362c1274
Nate <mpac4766@gmail.com>
2025-12-13 11:52:04 -0500

    Added comments to file

---
+++ nba_analysis/team_assigner/team_assigner.py
@@ -0,0 +1,74 @@
+from PIL import Image
+from transformers import CLIPProcessor, CLIPModel
+import cv2
+
+
+
+class TeamAssigner:
+    def __init__(self,
+                 team_1_class_name="white shirt",
+                 team_2_class_name="dark blue shirt"
+                 ):
+        
+        self.team_1_class_name=team_1_class_name
+        self.team_2_class_name=team_2_class_name
+
+        self.player_team_dict = {}
+
+
+    
+    def load_model(self):
+        self.model = CLIPModel.from_pretrained("patrickjohncyh/fashion-clip")
+        self.processor = CLIPProcessor.from_pretrained("patrickjohncyh/fashion-clip")
+    
+
+    def get_player_color(self, frame, bbox):
+        # bbox --> x1,y1,x2,y2
+        image = frame[int(bbox[1]):int(bbox[3]), int(bbox[0]):int(bbox[2])]
+
+        rgb_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        pil_image = Image.fromarray(rgb_image)
+
+        classes = [self.team_1_class_name, self.team_2_class_name]
+
+        inputs = self.processor(text=classes, images=pil_image, return_tensors="pt", padding=True)
+
+        outputs = self.model(**inputs)
+        logits_per_image = outputs.logits_per_image # this is the image-text similarity score
+        probs = logits_per_image.softmax(dim=1)
+
+        # Converting scores to probailities and return best match
+        class_name = classes[probs.argmax(dim=1)[0]]
+        return class_name
+
+    def get_player_team(self, frame, player_bbox, player_id):
+
+        if player_id in self.player_team_dict:
+             return self.player_team_dict[player_id] 
+
+        player_color = self.get_player_color(frame, player_bbox)
+
+        team_id = 2
+        if player_color ==self.team_1_class_name:
+            team_id = 1
+
+        self.player_team_dict[player_id]=team_id
+        return team_id
+    
+    def get_player_teams_across_frames(self, video_frames, player_tracks, read_from_stub=False, stub_path=None):
+
+        self.load_model()
+
+        player_assignment = []
+
+
+
+        for frame_num, player_track in enumerate(player_tracks):
+            player_assignment.append({})
+
+
+            for player_id, track in player_track.items():
+                team = self.get_player_team(video_frames[frame_num], track["bbox"], player_id)
+                player_assignment[frame_num][player_id]=team
+
+        return player_assignment
